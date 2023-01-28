0b104f31914483a387d9cdbc299bc66707786ccf
Gavin Fogel <gavin@fogel.io>
2023-01-27 20:09:26 -0500

    Animated logo fade in

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			animation: {
+				'fade-in': 'fadeIn 2s ease-in-out',
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+			},
 			fontFamily: {
 				mona: ['Mona-Sans', 'sans-serif'],
 				hubot: ['Hubot-Sans', 'sans-serif'],
