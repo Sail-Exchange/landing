2fafd88bed0ef3929b4fea489bfe74cae564e16d
Walter-Phillips <phillips.walter.n@gmail.com>
2023-08-17 13:29:42 -0700

    will it work this time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -9,8 +8,11 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   output: "server",
   adapter: vercel()
 });
